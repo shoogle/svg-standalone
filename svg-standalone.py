24eae49edd0a5356eedc7d0cc33cedd17a8f9365
Peter Jonas <shoogle@users.noreply.github.com>
2018-07-14 04:19:16 +0100

    Basic support for linked SVGs

--- svg-standalone.py
+++ svg-standalone.py
@@ -33,7 +33,13 @@ import os
 import re
 
 class SVG:
+    namespaces = {
+        "svg"   : "http://www.w3.org/2000/svg",
+        "xlink" : "http://www.w3.org/1999/xlink"
+    }
+
     def __init__(self, file_path):
+        self.register_namespaces(self.namespaces, "svg")
         self.file_path = file_path
         self.directory = os.path.dirname(file_path)
         self.tree = ET.parse(file_path)
@@ -41,15 +47,21 @@ class SVG:
         self.root.tail = "\n"
         self.parent_map = {c:p for p in self.root.iter() for c in p}
 
+    def register_namespaces(self, namespaces, default_ns=None):
+        for prefix, uri in namespaces.items():
+            if prefix == default_ns:
+                prefix = ""
+            ET.register_namespace(prefix, uri)
+
     def get_indentation(self, element, parent, element_index):
         assert(parent[element_index] is element)
         if element_index == 0:
-            match = re.match(r"[\r\n]([^\r\n]*)$", parent.text)
+            match = re.match(r"[\r\n]([ \t]*)$", parent.text)
             indentation = match[1] if match else ""
         else:
-            match = re.match(r"^([^\r\n]*)[\r\n]", parent[element_index-1].text)
+            match = re.match(r"^([ \t]*)[\r\n]", parent[element_index-1].text)
             indentation = match[1] if match else ""
-        return ""
+        return indentation
 
     def add_indentation(self, element, indentation):
         for child in element.iter():
@@ -59,8 +71,8 @@ class SVG:
                 child.tail += indentation
 
     def make_standalone(self):
-        for image in self.root.findall("image[@href]"):
-            file_path = image.get("href")
+        for image in self.root.findall(".//svg:image[@xlink:href]", self.namespaces):
+            file_path = image.get("{{{xlink}}}href".format_map(self.namespaces))
             if file_path.startswith("data:"):
                 continue
             if not os.path.isabs(file_path):
@@ -72,16 +84,18 @@ class SVG:
                 indentation = self.get_indentation(image, parent, index)
                 parent.remove(image)
                 r = SVG(file_path)
+                for dimension in ["x", "y", "width", "height"]:
+                    r.root.set(dimension, image.get(dimension, "0"))
                 self.add_indentation(r.root, indentation)
                 r.root.tail = image.tail
                 parent.insert(index, r.root)
             else:
                 with open(file_path, "rb") as image_file:
                     encoded_string = base64.b64encode(image_file.read())
-                    image.set("href", "data:image/" + extension + ";base64," + encoded_string.decode('utf-8'))
+                    image.set("{{{xlink}}}href".format_map(self.namespaces), "data:image/" + extension + ";base64," + encoded_string.decode('utf-8'))
 
     def write_to_file(self, file):
-        self.tree.write(file, encoding="UTF-8", xml_declaration="True")
+        self.tree.write(file, encoding="UTF-8")
 
 for file in args.svg:
     s = SVG(file)
